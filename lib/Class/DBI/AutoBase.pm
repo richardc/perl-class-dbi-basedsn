538c7aebc9e3f41fc58b6c2becb186a44e6892a3
richardc <richardc@c56e198b-7db7-0310-b3ab-8e406f684e33>
2003-06-09 10:52:19 +0000

    grand rename
    git-svn-id: file:///web/svn.unixbeard.net/repo/richardc/perl/Class-DBI-BaseDSN@1203 c56e198b-7db7-0310-b3ab-8e406f684e33

---
+++ lib/Class/DBI/AutoBase.pm
@@ -0,0 +1,88 @@
+use strict;
+package Class::DBI::AutoBase;
+use vars qw($VERSION);
+$VERSION = '1.21';
+
+sub set_db {
+    my $package = shift;
+    my ($name, $dsn) = @_;
+
+    my $isa = do { no strict 'refs'; \@{"$package\::ISA"} };
+    die "$package is not directly a ".__PACKAGE__
+      unless grep { __PACKAGE__ eq $_ } @$isa;
+
+    $dsn =~ m/^(?:dbi:)?([^:]+)/i
+      or die "couldn't identify a backend from $dsn";
+
+    my $backend = "Class::DBI::$1";
+
+    unless (eval "require $backend; 1") {
+        $@ =~ /^Can't locate/ or die $@;
+
+        # if it simply wasn't there fall back to Class::DBI
+        $backend = 'Class::DBI';
+        require Class::DBI;
+    }
+
+    # okay, get out of the way, and make like we were never here
+    for (@$isa) {
+        $_ = $backend if $_ eq __PACKAGE__;
+    }
+
+    my $method = $package->can('set_db');
+    unshift @_, $package;
+    goto &$method;
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Class::DBI::AutoBase - automatically set your base class to reflect the database you specified
+
+=head1 SYNOPSIS
+
+  package My::DBI;
+  use base 'Class::DBI::AutoBase'; # we'll decide later what our real
+                                   # parent class will be
+  __PACKAGE__->set_db( Main => $ENV{TESTING} ? @test_dsn : @real_dsn );
+
+
+=head1 DESCRIPTION
+
+Class::DBI::AutoBase acts as a placeholder for a base class which will
+be switched for a specific Class::DBI extension when you specify the
+dsn of the database to connect to.
+
+For example in this case, the Class::DBI::AutoBase will replace itself
+with Class::DBI::mysql when the C<set_db> call is executed.
+
+ package Example::DBI;
+ use base 'Class::DBI::AutoBase';
+ __PACKAGE__->set_db( Main => 'dbi:mysql:example', 'user', 'pass' );
+
+Since this happens at runtime you could pass the dsn as a variable and
+so have it use a completely different extension automatically.  This
+is especially useful for testing, or for applications where dsn may be
+a configuration option.
+
+If there is no matching extension found, Class::DBI::AutoBase replaces
+itself with Class::DBI.
+
+=head1 AUTHOR
+
+Richard Clamp <richardc@unixbeard.net>
+
+=head1 THANKS
+
+Thanks go to Michael Schwern for a snippet to fake out caller, and for
+hashing out some of the finer points of the approach on the cdbi-talk
+list.
+
+=head1 SEE ALSO
+
+L<Class::DBI>
+
+=cut
---
+++ t/ateam.t
@@ -0,0 +1,31 @@
+#!perl -w
+use strict;
+use lib qw(t/lib);
+use Test::More tests => 5;
+
+my $pkg;
+BEGIN { $pkg = 'Class::DBI::AutoBase' };
+use_ok( $pkg );
+package Foo;
+use base $pkg;
+my $foo;
+__PACKAGE__->set_db('Main', 'dbi:ATeam', \$foo);
+
+package main;
+is( $foo, 'Foo, you have found the A-team',
+    "redispatched to the A-Team set_db" );
+is( $Foo::ISA[0], 'Class::DBI::ATeam', 'Foo @ISA' );
+
+package Bar;
+use base $pkg;
+__PACKAGE__->set_db('Main', 'dbi:AbsentAndUnlikey' );
+
+package main;
+is( $Bar::ISA[0], 'Class::DBI', 'Fallback to Class::DBI on absent class' );
+
+package Baz;
+use base $pkg;
+eval { __PACKAGE__->set_db('Main', 'dbi:CompileError' ) };
+
+package main;
+like( $@, qr/^Global symbol "\$foo"/, 'error blows on compile error');
